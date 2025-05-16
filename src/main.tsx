f76bff3046a0aa765d7470e1cd15bdbd1a494673
Nishant <nishant.dahiya2000@gmail.com>
2025-05-16 19:33:39 +0530

    implement global theme-provider and theme-toggle

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import { Button } from "@/components/ui/button";
+import { ThemeProvider } from "./components/ui/theme-provider";
+import { ModeToggle } from "./components/ui/theme-toggle";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <h1 className="font-bold bg-black text-white">
-      This is just a simple text message
-    </h1>
-  </React.StrictMode>,
+  <ThemeProvider>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button onClick={() => console.log("Clicked me!")}>Click me</Button>
+    </div>
+    <ModeToggle />
+  </ThemeProvider>,
 );
