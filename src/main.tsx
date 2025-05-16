5241f75f9444591d3602a98d8f13aa82ae5b3a48
Nishant <nishant.dahiya2000@gmail.com>
2025-05-16 18:19:13 +0530

    batman commit

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <h1 className="font-bold bg-black text-white">
+      This is just a simple text message
+    </h1>
+  </React.StrictMode>,
+);
