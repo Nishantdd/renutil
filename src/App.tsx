0927ad9e48b7e34b3029a89442f1e1a9505e5dc2
Nishantdd <nishant.dahiya2000@gmail.com>
2025-12-08 21:54:39 +0530

    remove responsive logic from app as min height and width are enforced

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,14 @@ import ActionCards from "./components/ActionCards";
 
 export default function App() {
   return (
-    <div className="grid grid-cols-1 grid-rows-4 sm:grid-cols-[1fr_3fr] sm:grid-rows-[auto_1fr] h-screen w-screen">
-      <div className="bg-secondary border-b sm:border-r border-border">
+    <div className="grid grid-cols-[1.5fr_3fr] grid-rows-[auto_1fr] h-screen w-screen">
+      <div className="bg-secondary border-b border-r border-border">
         <FolderInput />
       </div>
       <div className="flex bg-secondary items-center justify-end border-b border-border">
         <ThemeToggle />
       </div>
-      <div className="flex flex-col bg-card border-b sm:border-b-0 sm:border-r border-border">
+      <div className="flex flex-col bg-card border-b-0 border-r border-border">
         <ActionCommandMenu />
         <ActionCards />
       </div>
