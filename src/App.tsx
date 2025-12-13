db490e4be5792972bc5ddaad2a17126c4ee76588
Nishant <nishant.dahiya2000@gmail.com>
2025-12-13 15:53:52 +0530

    make column one responsive with window size

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import ActionCards from "./components/ActionCards";
 
 export default function App() {
   return (
-    <div className="grid grid-cols-[340px_1fr] grid-rows-[auto_1fr] h-screen w-screen">
+    <div className="grid grid-cols-[minmax(240px,1fr)_4fr] grid-rows-[auto_1fr] h-screen w-screen">
       <div className="bg-secondary border-b border-r border-border">
         <FolderInput />
       </div>
