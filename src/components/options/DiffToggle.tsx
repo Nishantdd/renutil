cb1d71d1e96e603c0f41fc11e2d50937b3c8d9a1
Nishant <nishant.dahiya2000@gmail.com>
2026-01-03 15:01:28 +0530

    replace button component with toggle in diff toggle

--- src/components/options/DiffToggle.tsx
+++ src/components/options/DiffToggle.tsx
@@ -1,18 +1,16 @@
 import { useOptionStore } from "@/store/optionStore";
 import { Diff } from "lucide-react";
-import { Button } from "../ui/button";
+import { Toggle } from "../ui/toggle";
 
 export function DiffToggle() {
   const toggleShowDiff = useOptionStore((s) => s.toggleShowDiff);
   return (
-    <Button
-      variant="ghost"
-      size="icon"
+    <Toggle
       className="border-l group"
       onClick={toggleShowDiff}
       aria-label="Toggle diff"
     >
       <Diff className="group-active:scale-90" />
-    </Button>
+    </Toggle>
   );
 }
